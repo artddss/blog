3d8547578ac15d8482230b2985af6a546cba9181
artddss <artddss@gmail.com>
2020-08-30 12:33:11 +0500

    create PostController, PostFormRequest

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        //fetch 5 posts from database which are active and latest
+        $posts = Posts::where('active',1)->orderBy('created_at','desc')->paginate(5);
+        //page heading
+        $title = 'Latest Posts';
+        //return home.blade.php template from resources/views folder
+        return view('home')->withPosts($posts)->withTitle($title);
+    }
+
+    public function create(Request $request)
+    {
+        //
+        if ($request->user()->can_post()) {
+            return view('posts.create');
+        } else {
+            return redirect('/')->withErrors('You have not sufficient permissions for writing post');
+        }
+    }
+
+    public function store(PostFormRequest $request)
+    {
+        $post = new Posts();
+        $post->title = $request->get('title');
+        $post->body = $request->get('body');
+        $post->slug = Str::slug($post->title);
+
+        $duplicate = Posts::where('slug', $post->slug)->first();
+        if ($duplicate) {
+            return redirect('new-post')->withErrors('Title already exists.')->withInput();
+        }
+
+        $post->author_id = $request->user()->id;
+        if ($request->has('save')) {
+            $post->active = 0;
+            $message = 'Post saved successfully';
+        } else {
+            $post->active = 1;
+            $message = 'Post published successfully';
+        }
+        $post->save();
+        return redirect('edit/' . $post->slug)->withMessage($message);
+    }
+
+
+}
