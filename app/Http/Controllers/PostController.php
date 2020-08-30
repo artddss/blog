a0aad10e560f7255d70db2bfe9451bc85040b1c1
artddss <artddss@gmail.com>
2020-08-30 12:42:10 +0500

    PostConteroller to add function store, show, edit, udpate, destroy

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,7 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+
+use App\Posts;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\PostFormRequest;
+
+class PostController extends Controller {
+//other code/ functions below
 
 class PostController extends Controller
 {
@@ -48,6 +56,77 @@ class PostController extends Controller
         }
         $post->save();
         return redirect('edit/' . $post->slug)->withMessage($message);
+    }
+
+    public function show($slug)
+    {
+        $post = Posts::where('slug',$slug)->first();
+        if(!$post)
+        {
+            return redirect('/')->withErrors('requested page not found');
+        }
+        $comments = $post->comments;
+        return view('posts.show')->withPost($post)->withComments($comments);
+    }
+
+    public function edit(Request $request,$slug)
+    {
+        $post = Posts::where('slug',$slug)->first();
+        if($post && ($request->user()->id == $post->author_id || $request->user()->is_admin()))
+            return view('posts.edit')->with('post',$post);
+        return redirect('/')->withErrors('you have not sufficient permissions');
+    }
+
+    public function update(Request $request)
+    {
+        //
+        $post_id = $request->input('post_id');
+        $post = Posts::find($post_id);
+        if ($post && ($post->author_id == $request->user()->id || $request->user()->is_admin())) {
+            $title = $request->input('title');
+            $slug = Str::slug($title);
+            $duplicate = Posts::where('slug', $slug)->first();
+            if ($duplicate) {
+                if ($duplicate->id != $post_id) {
+                    return redirect('edit/' . $post->slug)->withErrors('Title already exists.')->withInput();
+                } else {
+                    $post->slug = $slug;
+                }
+            }
+
+            $post->title = $title;
+            $post->body = $request->input('body');
+
+            if ($request->has('save')) {
+                $post->active = 0;
+                $message = 'Post saved successfully';
+                $landing = 'edit/' . $post->slug;
+            } else {
+                $post->active = 1;
+                $message = 'Post updated successfully';
+                $landing = $post->slug;
+            }
+            $post->save();
+            return redirect($landing)->withMessage($message);
+        } else {
+            return redirect('/')->withErrors('you have not sufficient permissions');
+        }
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        //
+        $post = Posts::find($id);
+        if($post && ($post->author_id == $request->user()->id || $request->user()->is_admin()))
+        {
+            $post->delete();
+            $data['message'] = 'Post deleted Successfully';
+        }
+        else
+        {
+            $data['errors'] = 'Invalid Operation. You have not sufficient permissions';
+        }
+        return redirect('/')->with($data);
     }
 
 
